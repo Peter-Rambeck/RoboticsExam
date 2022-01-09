ad98af9c8bf8a9fbf7de7f7d11cd9f9d490e72a6
Jens Gelbek <jensgelbek@gmail.com>
2022-01-09 17:09:22 +0100

    latest files

--- NemaController.java
+++ NemaController.java
@@ -2,16 +2,34 @@ package org.abstractica.openbuildsystem.Nema17Controller;
 
 public class NemaController {
 
-    public void turn_left(){
-        Mqqt_Publisher.mqqtNemaPublisher("turn left");
+    public void turn_clockwise(int position,int speed){
+        Mqqt_Publisher.mqqtNemaPublisher("+",position,speed);
     }
-    public void turn_right(){
-        Mqqt_Publisher.mqqtNemaPublisher("turn right");
+    public void turn_counterclockwise(int position,int speed){
+        Mqqt_Publisher.mqqtNemaPublisher("-",position,speed);
     }
-    public static void main(String[] args){
+    public void turn_1rund_clockwise(){
+        Mqqt_Publisher.mqqtNemaPublisher("+",800,200);
+    }
+    public void turn_05rund_clockwise(){
+        Mqqt_Publisher.mqqtNemaPublisher("+",400,200);
+    }
+    public void turn_025rund_clockwise(){
+        Mqqt_Publisher.mqqtNemaPublisher("+",200,200);
+    }
+    public void turn_1rund_counterclockwise(){
+        Mqqt_Publisher.mqqtNemaPublisher("-",800,200);
+    }
+    public void turn_05rund_counterclockwise(){
+        Mqqt_Publisher.mqqtNemaPublisher("-",400,200);
+    }
+    public void turn_025rund_counterclockwise(){
+        Mqqt_Publisher.mqqtNemaPublisher("-",200,200);
+    }
+     public static void main(String[] args){
         NemaController nc=new NemaController();
-        nc.turn_left();
-        nc.turn_right();
+        nc.turn_1rund_counterclockwise();
+        nc.turn_clockwise(400,234);
     }
 
 }
